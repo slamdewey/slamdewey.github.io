038ac0d1f61f5b680b4ca6cee36dc978aa114cf2
Jared Massa <mr.jaredmassa@gmail.com>
2025-07-15 20:39:25 -0400

    ci(format): config updates for prettier

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import angular from "angular-eslint";
+import prettier from "eslint-config-prettier";
+
+export default [
+  ...eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  ...angular.configs.tsRecommended,
+  ...prettier,
+  {
+    files: ["**/*.ts"],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", prefix: "x", style: "camelCase" },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", prefix: "x", style: "kebab-case" },
+      ],
+    },
+  },
+  ...angular.configs.templateRecommended,
+  ...angular.configs.templateAccessibility,
+  {
+    files: ["**/*.html"],
+    rules: {},
+  },
+];
