dea3915133480d6e130fa735537d6ccbad292d67
Jared Massa <mr.jaredmassa@gmail.com>
2025-07-16 23:11:46 -0400

    config(eslint): possible last fix for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,30 @@
+import { defineConfig } from "eslint/config";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import angular from "angular-eslint";
 import prettier from "eslint-config-prettier";
+import unusedImports from 'eslint-plugin-unused-imports';
 
-export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
-  ...angular.configs.tsRecommended,
-  ...prettier,
+export default defineConfig([
   {
-    files: ["**/*.ts"],
-    processor: angular.processInlineTemplates,
+    ignores: ["dist/", "node_modules/"]
+  },
+  {
+    files: ["src/**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      prettier,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: {
+      "unused-imports": unusedImports
+    },
     rules: {
+      "no-case-declarations": "off", // what's the point of this rule? to put functions everywhere?
+      "@typescript-eslint/no-empty-function": "off", // required for virtual functions
+      "@typescript-eslint/no-explicit-any": "off", // required for complex marshalling
       "@angular-eslint/directive-selector": [
         "error",
         { type: "attribute", prefix: "x", style: "camelCase" },
@@ -21,12 +33,29 @@ export default [
         "error",
         { type: "element", prefix: "x", style: "kebab-case" },
       ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          "vars": "all",
+          "varsIgnorePattern": "^_",
+          "args": "after-used",
+          "argsIgnorePattern": "^_"
+        }
+      ]
     },
+    processor: angular.processInlineTemplates,
   },
-  ...angular.configs.templateRecommended,
-  ...angular.configs.templateAccessibility,
   {
-    files: ["**/*.html"],
+    files: ["src/**/*.html"],
+    extends: [
+      eslint.configs.recommended,
+      prettier,
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
     rules: {},
   },
-];
+]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'x-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   imports: [NavigationComponent, RouterOutlet],
